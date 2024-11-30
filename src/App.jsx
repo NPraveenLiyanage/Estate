0b89a4b4c97873fc5b0913f1df056b889322c380
NPraveenLiyanage <lnipuna20@gmail.com>
2024-11-30 22:34:13 +0530

    add controller, router, model files

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import SignUP from "./pages/SignUP";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/Signin" element={<Signin />}/>
